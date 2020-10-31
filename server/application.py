29ba841a5b0c274197972774fd0ea3a6e2c5bfb8
jmather625 <jatinm2@illinois.edu>
2020-10-31 13:34:39 -0500

    switched to sessions instead of variables, code works for two concurrent users

--- server/application.py
+++ server/application.py
@@ -3,17 +3,13 @@ import json
 import logging
 import flask
 
+from sessions import create_new_session, end_session, get_session_data, insert_session_data
 import sys
 sys.path.append("../")
 from elicit import ElectoralElicitation
 
 
 application = flask.Flask(__name__)
-
-
-# using global for now
-ee = ElectoralElicitation(50)
-cur_trial = None
 
 
 def option_to_string(option: int):
@@ -25,24 +21,21 @@ def option_to_string(option: int):
         return "ERROR"
 
 
-def generate_vote_data():
-    global ee, cur_trial
+def generate_vote_data(ee: ElectoralElicitation):
     vd = ee.generate_trial()
-    cur_trial = vd
     states = ['Vermont', 'Ohio', 'South Dakota', 'Alaska', 'Michigan', 'Oklahoma', 'Mississippi', 'Virginia', 'Texas', 'Arkansas', 'Utah', 'Connecticut', 'Delaware', 'Florida', 'Indiana', 'Maryland', 'New Hampshire', 'Arizona', 'New Mexico', 'Nebraska', 'Kansas', 'Louisiana', 'Tennessee', 'Maine', 'Rhode Island', 'Colorado', 'Idaho', 'North Carolina', 'Georgia', 'Kentucky', 'New Jersey', 'West Virginia', 'Massachusetts', 'Iowa', 'Hawaii', 'Washington', 'Alabama', 'Wisconsin', 'California', 'North Dakota', 'Pennsylvania', 'Wyoming', 'South Carolina', 'New York', 'Illinois', 'Minnesota', 'Montana', 'Nevada', 'Missouri', 'Oregon']
-    vote_data = dict()
+    vd_dict = dict()
     for i in range(50):
         s = states[i].upper()
-        vote_data[s] = {
+        vd_dict[s] = {
             "h": vd.stoh[i],
             "dv": vd.stodv[i],
         }
-    return vote_data
+    return vd_dict, vd
 
 
 @application.route('/', methods=['GET', 'POST'])
 def home():
-    global ee, cur_trial
     if flask.request.method == 'POST':
         data = None
         try:
@@ -55,17 +48,24 @@ def home():
             logging.error("Invalid payload")
             resp = flask.make_response("Error: invalid payload", 400)
             return resp
-
+        
+        if 'sid' not in flask.session:
+            resp = flask.make_response("Error: never started the survey", 400)
+            return resp
+ 
+        sid = flask.session['sid']
+        ret = get_session_data(sid)
+        ee = ret[0]
+        cur_vd = ret[1]
         if data["winner"] == "D":
-            ee.process_trial(cur_trial, True)
+            ee.process_trial(cur_vd, True)
         else:
-            ee.process_trial(cur_trial, False)
+            ee.process_trial(cur_vd, False)
 
         if ee.converged():
             o_hat = ee.predict_opt()
             o_hat_str = option_to_string(o_hat)
             f_hat = round(ee.predict_f(), 2)
-            ee.clear()
             return json.dumps(
                 {
                     'option': o_hat_str,
@@ -73,14 +73,20 @@ def home():
                 }
             )
 
-        vote_data = generate_vote_data()
-        return json.dumps(vote_data)
+        vd_dict, cur_vd = generate_vote_data(ee)
+        ret[-1] = cur_vd # update stored vd
+        return json.dumps(vd_dict)
 
-        
-    vote_data = generate_vote_data()
+    # start a new session
+    sid = create_new_session()
+    flask.session['sid'] = sid
+    ee = ElectoralElicitation(50) 
+    vd_dict, cur_vd = generate_vote_data(ee)
+    insert_session_data(sid, [ee, cur_vd])
+
     fp = open('shapes/usa2.json', 'rb')
     context = {
-        'vote_data': vote_data,
+        'vote_data': vd_dict,
         'map_json': json.load(fp),
 
     }
@@ -90,6 +96,7 @@ def home():
 
 # run the application.
 if __name__ == "__main__":
+    application.secret_key = 'yeet'
     application.debug = True
     application.run(host="0.0.0.0", threaded=True)
 
---
+++ server/sessions.py
@@ -0,0 +1,35 @@
+import threading
+
+LOCK = threading.Lock()
+
+# store session info on everyone
+SESSION_DATA = dict()
+MAX_ID = 0
+
+
+def create_new_session() -> int:
+    global MAX_ID
+    LOCK.acquire()
+    new_id = MAX_ID
+    MAX_ID += 1
+    LOCK.release()
+    return new_id
+
+
+def get_session_data(sid: int):
+    LOCK.acquire()
+    v = SESSION_DATA[sid]
+    LOCK.release()
+    return v
+
+
+def insert_session_data(sid: int, data):
+    LOCK.acquire()
+    SESSION_DATA[sid] = data
+    LOCK.release()
+
+
+def end_session(sid: int):
+    LOCK.acquire()
+    if sid in SESSION_DATA:
+        del SESSION_DATA[sid]
